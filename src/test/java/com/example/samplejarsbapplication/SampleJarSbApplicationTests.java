3bc7089c8597e186ad1afb5c86b831f142ad002f
Ravi Kumar Gupta <babaravibbb@gmail.com>
2023-09-30 11:53:39 +0530

    SampleJarSBApplication

---
+++ src/test/java/com/example/samplejarsbapplication/SampleJarSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplejarsbapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJarSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
